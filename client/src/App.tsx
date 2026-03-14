1dec3599c7cc6e29dd738eeae5ccb273d3c740cf
CAPFlyingFun <105285920+CAPFlyingFun@users.noreply.github.com>
2026-03-14 14:26:12 -0500

    Add client/src/App.tsx

---
+++ client/src/App.tsx
@@ -0,0 +1,31 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import StormTracker from "@/pages/storm-tracker";
+import MessageInbox from "@/pages/message-inbox";
+import NotFound from "@/pages/not-found";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={StormTracker} />
+      <Route path="/messages" component={MessageInbox} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Router />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
